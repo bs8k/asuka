a0b5989b1e0b252005751a24c331fe99bb404209
bs8k <lomeyuki_1123@yahoo.co.jp>
2026-09-09 16:07:34 +0900

    Update and rename main.js to test2.md

--- main.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('h1').style.backgroundColor = 'blue';
-document.querySelectorAll('p').forEach(p => {
-  if (p.textContent.trim() === '管理者') {
-    p.innerHTML = '<small id="fcopy">&copy;&#32;&#99;&#104;&#105;&#110;&#111;&#100;</small>';
-  }
-});
