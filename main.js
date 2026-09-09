8a5b1de68f355dac3b4be2ddbad62f17eb5254c6
bs8k <lomeyuki_1123@yahoo.co.jp>
2026-09-09 11:56:17 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
+document.querySelector('h1').style.backgroundColor = 'blue';
 document.querySelectorAll('p').forEach(p => {
   if (p.textContent.trim() === '管理者') {
-    p.textContent = '&copy; bs8k';
+    p.innerHTML = '&lt;&#115;&#109;&#97;&#108;&#108;&gt;&copy;&#32;&#99;&#104;&#105;&#110;&#111;&#100;&lt;&#47;&#115;&#109;&#97;&#108;&#108;&gt;';
   }
 });
