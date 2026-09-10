99e9be90a88e12a564097d13e7096f236e70ea7f
bs8k <lomeyuki_1123@yahoo.co.jp>
2026-09-10 16:57:43 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,12 @@
 document.querySelector('h1').style.backgroundColor = 'skyblue';
-document.getElementById('fcopy').innerHTML = '<small>&copy;&#32;chinodigital</small>';
+//document.getElementById('fcopy').innerHTML = '<small>&copy;&#32;chinodigital</small>';
+document.body.insertAdjacentHTML('beforeend', '<small>&copy;&#32;chinodigital</small>');
 
 // 画像ビューア表示
-const viewer = document.getElementById("viewer");
-const viewerImg = document.getElementById("viewer-img");
+const viewer = document.getElementById('viewer');
+const viewerImg = document.getElementById('viewer-img');
 
-document.querySelectorAll("p img").forEach(img => {
+document.querySelectorAll('p img').forEach(img => {
   img.onclick = () => {
     viewerImg.src = img.src;
     viewer.showModal();
